315d53e969ea4d717d32aa01d2647cbe42293981
Gianna Graziano <graziang1@hawkmail.newpaltz.edu>
2020-09-18 00:25:37 -0400

    getting npm set up

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World. You requested ' + req.url)
+  res.send('Hello Hudson Valley! You requested ' + req.url)
 })
 
 app.listen(port, () => {
