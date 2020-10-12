06140954c229bb5737705c0a3387f97e5a0d42b9
Gianna Graziano <graziang1@hawkmail.newpaltz.edu>
2020-10-11 21:44:42 -0400

    added basic design elements for feed

---
+++ client/src/views/Feed.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="page">
+        <h2 class="title.is-2">Feed Page</h2>
+        <div class="columns">
+            <div class="column is-two-thirds">
+                <Post /> <Post /> <Post /> <Post />
+            </div>
+            <div class="column is-one-third">
+                <Sidebar />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Sidebar from "@/components/Sidebar";
+import Sidebar from "@/components/Post";
+
+export default {
+    components:(
+        Sidebar, Post
+    )
+}
+</script>
+
+<style>
+    .card {
+        margin-bottom: 30px;
+    }
+</style>
