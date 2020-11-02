4e2bc31c3a90c9b67d3d501487368d75845e3eb9
Gianna Graziano <graziang1@hawkmail.newpaltz.edu>
2020-11-02 15:31:25 -0500

    added add, update & remove functions w/ routers

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -9,7 +9,7 @@ router
         .catch(next);
     })
     .get('/:id', (req, res, next) => { 
-        const id = +req.params.id; //+ converts id to a number
+        const id = +req.params.id; //+ converts id to a number request a parameter
         if(!id) return next(); //if id is not a number, try another function on the pipeline
         users.get(id).then(x=> res.send( x ) )
         .catch(next);
@@ -23,17 +23,31 @@ router
         .catch(next);
     })
     .post('/', (req, res, next) => {
-        users.add(req.query.name, req.query.age ).then(newUser => { //creates new user
+        users.add(
+            req.body.FirstName, 
+            req.body.LastName,
+            req.body.DOB,
+            req.body.Password,
+            6 /*User Type*/,
+        ).then(newUser => { //creates new user
             res.send( newUser ); //returns new user
         }).catch(next)
     })
-    /*
-    .get('/rand', (req, res, next) => {
-        users.rand()
-        .then(someVal => res.send({someVal}))
-        .catch(next); //send object with a someVal property random # to client
+    .put('/:id', (req, res, next) => {
+        users.update( req.params.id,
+            req.body.FirstName, 
+            req.body.LastName,
+            req.body.DOB,
+            req.body.Password,
+            6 /*User Type*/,
+        ).then(newUser => { //creates new user
+            res.send( newUser ); //returns new user
+        }).catch(next)
     })
-    */
-
+    .delete('/:id', (req, res, next) => {
+        users.remove( req.params.id).then(msg => { //creates new user
+            res.send( msg ); //returns new user
+        }).catch(next)
+    })
 module.exports = router;  //when this file is required, you'll get this router (return obj from file)
 
--- server/index.js
+++ server/index.js
@@ -8,6 +8,20 @@ const port = process.env.PORT || 3000;
 
 console.log(process.env.BEST_CLASS);
 
+//express passes every request to json function, which looks at body of request and parses
+//it if its valid json request and puts it into the body of the request (the body property)
+//creates a pipeline
+app.use(express.json()); 
+
+//middleware
+app.use(function(req, res, next) {
+  const arr = (req.headers.authorization || "").split(" ");
+  if(arr.length > 1 && arr[1] != null){
+    req.userID = +arr[1];
+  }
+  next();
+});
+
 app.get('/', (req, res, next) => {
   res.send('Hello Hudson Valley! You requested ' + req.url)
 })
