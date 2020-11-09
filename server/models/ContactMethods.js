3686db5851127f1f558c11506e105e1b7144d596
Gianna Graziano <graziang1@hawkmail.newpaltz.edu>
2020-11-08 22:59:06 -0500

    created register fuction

--- server/models/ContactMethods.js
+++ server/models/ContactMethods.js
@@ -1,4 +1,6 @@
 const mysql = require('./mysql');
+const PREFIX = process.env.MYSQL_TABLE_PREFIX || 'Fall2020_';
+const Types = { EMAIL: 'Email', CELL_PHONE:'Cell Phone' };
 
 //const data = [{ name: 'Moshe', age: 43}, { name: 'Biden', age: 78 }] //array of objects
 
@@ -6,36 +8,42 @@ async function getAll(){
     //throw { status: 501, message: "This is a fake error" }
     //await Promise.resolve()
     console.log("Called Get All")
-    return await mysql.query(`SELECT * FROM Users`);
+    return await mysql.query(`SELECT * FROM ${PREFIX}ContactMethods`);
 }
 
 async function get(id){
-    const rows = await mysql.query(`SELECT * FROM Users WHERE id = ?`,[id]);
+    const rows = await mysql.query(`SELECT * FROM ${PREFIX}ContactMethods WHERE id=?`, [id]);
     if(!rows.length) throw { status: 404, message: "Sorry, there is no such user" };
-    return rows;
+    return rows[0];
+}
+
+async function exists(email){
+    const rows = await mysql.query(`SELECT * FROM ${PREFIX}ContactMethods WHERE Value=?`, [email]);
+    console.log ( { rows })
+    return rows.length;
 }
 
 async function getTypes(){
-    return await mysql.query(`SELECT id, Name FROM Types WHERE Type_id = 2`);
+    return await mysql.query(`SELECT id, Name FROM ${PREFIX}Types WHERE Type_id = 4`);
 }
 
-async function add(FirstName, LastName, DOB, Password, User_Type){
-    const sql = 'INSERT INTO `Users` (`created_at`, `FirstName`, `LastName`, `DOB`, `Password`, `User_Type`) VALUES ? ;';
-    const params = [[new Date(), FirstName, LastName, new Date(DOB), Password, User_Type]];
+async function add(Type, Value, IsPrimary = 0, CanSpam = 1, User_id){
+    const sql = `INSERT INTO ${PREFIX}ContactMethods (created_at, Type, Value, IsPrimary, CanSpam, User_id) VALUES ? ;`;
+    const params = [[new Date(), Type, Value, IsPrimary, CanSpam, User_id]];
     return await mysql.query(sql, [params]);
 }
 
-async function update(id, FirstName, LastName, DOB, Password, User_Type){
-    const sql = 'UPDATE `Users` SET ? WHERE `id` = ?';
-    const params = { FirstName, LastName, DOB: new Date(DOB), Password, User_Type };
+async function update(id, Type, Value, IsPrimary, CanSpam, User_id){
+    const sql = `UPDATE ${PREFIX}ContactMethods SET ? WHERE id = ?;`;
+    const params = { Type, Value, IsPrimary, CanSpam, User_id };
     return await mysql.query(sql, [params, id]);
 }
 
 async function remove(id){
-    const sql = 'DELETE FROM `Users` WHERE `Users`.`id` = ?';
+    const sql = `DELETE FROM ${PREFIX}ContactMethods WHERE id = ?`;
     return await mysql.query(sql, [id]);
 }
 
-const search = async q => await mysql.query(`SELECT id, FirstName, LastName FROM Users WHERE LastName LIKE ? OR FirstName LIKE ?; `, [`%${q}%`, `%${q}%`]);
+const search = async q => await mysql.query(`SELECT id, Value FROM ${PREFIX}ContactMethods WHERE Value LIKE ?; `, [`%${q}%`]);
 
-module.exports = { getAll, get, add, update, remove, getTypes, search }
+module.exports = { getAll, get, exists, add, update, remove, getTypes, search, Types }
