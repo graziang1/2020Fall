7aca3b71a6fd04a498b59fbc1eea61edc0e2d9b3
Gianna Graziano <graziang1@hawkmail.newpaltz.edu>
2020-11-15 03:37:53 -0500

    client server communication

--- server/index.js
+++ server/index.js
@@ -1,51 +1,53 @@
+//pipeline - request comes in on one side and goes through
 const express = require('express');
 const path = require('path');
-require('dotenv').config(); //to utilize the .env file with the new port #
+require('dotenv').config();
 
-const users = require('./controllers/users'); //makes users a controller
+const users = require('./controllers/users');
 
 const app = express()
 const port = process.env.PORT || 3000;
 
 console.log(process.env.BEST_CLASS);
 
-//express passes every request to json function, which looks at body of request and parses
-//it if its valid json request and puts it into the body of the request (the body property)
-//creates a pipeline
-//Middleware
-app.use(express.json()); 
-app.use(express.static(__dirname + '/../docs')); //will serve files straight off your computer (hosting static files)
+//  Middleware
+app.use(express.json());
+app.use(express.static( __dirname + '/../docs/'))
 
-//Authentication/Authorization
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+//  Authentication
 app.use(function(req, res, next) {
   const arr = (req.headers.authorization || "").split(" ");
   if(arr.length > 1 && arr[1] != null){
-    req.userID = +arr[1];
+      req.userId = +arr[1];
   }
   next();
 });
 
-//API
+//  API
 app.get('/hello', (req, res, next) => {
   res.send('Hello Hudson Valley! You requested ' + req.url)
 })
 
-//add to the pipeline: if it matches ./users, then it'll be passed to the users controller
-//mount the users controller at users
 app.use('/users', users);
 
 app.get('*', (req, res, next) => {
-  const filename = path.join(__dirname + '/../docs/index.html');
-  console.log(filename);
-  res.sendFile( filename );
+    const filename = path.join(__dirname, '/../docs/index.html');
+    console.log(filename);
+    res.sendFile( filename );
 })
 
-//error handler
-app.use((err, req, res, next) =>{
-  console.log(err);
-  res.status(err.status || 500).send( { message: err.message } )
-})
+app.use( (err, req, res, next) =>{
+    console.log(err);
+    res.status(err.status || 500).send( { message: err.message } )
+} )
 
+//  Init
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
