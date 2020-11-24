ed23bdc64cc1a800a31081dfbb3a4e7830c6a837
Gianna Graziano <graziang1@hawkmail.newpaltz.edu>
2020-11-24 01:15:30 -0500

    posting to the server

--- client/src/models/feed.js
+++ client/src/models/feed.js
@@ -1,11 +1,20 @@
 import { myFetch } from "./my-fetch";
+import session from "./session";
 
 /*
     Feed Model. The raw data for the feed display.
     Currently mocked at the client side.
 */
-export const getPosts = ()=>  myFetch('posts');
+export const getPosts = ()=> myFetch(`posts/byUser/1`);
 
+export function react(post_id){
+    //console.log(session.user)
+    return myFetch('reactions', { Post_id: post_id, Owner_id: 1 })
+}
+
+export function comment(post_id, text){
+    return myFetch('comments', { Post_id: post_id, Owner_id: 1, Text: text })
+}
 /*
 export const posts = [
     {
